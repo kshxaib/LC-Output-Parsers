b0725f7d3bc235a75f767bc1930314345893f6f8
Khan Shoaib <khanshoaibishtiyak@gmail.com>
2026-08-10 13:11:34 +0530

    10 Aug

--- jsonoutputparser.py
+++ jsonoutputparser.py
@@ -3,24 +3,42 @@ from langchain_core.output_parsers import JsonOutputParser
 from langchain_core.prompts import PromptTemplate
 from dotenv import load_dotenv
 
-
 load_dotenv()
 
 model = ChatOpenAI(model="gpt-3.5-turbo")
 
+
+# JsonOutputParser → converts the LLM's JSON response
+# into a Python dictionary
 parser = JsonOutputParser()
 
+
 template = PromptTemplate(
-    template="Give me the name, age and city of a famous Indian Cricketer. /n {farmat_instruction}",
+    template="""
+    Give me the name, age and city of a famous Indian Cricketer.
+
+    {format_instruction}
+    """,
+
+    # No normal input variables because the prompt
+    # does not require user-provided values
     input_variables=[],
-    partial_variables = {'farmat_instruction': parser.get_format_instructions()}
+
+    # Injects instructions telling the LLM
+    # what JSON format it should return
+    partial_variables={
+        'format_instruction': parser.get_format_instructions()
+    }
 )
 
-# prompt = template.format()
-# response = model.invoke(prompt)
-# output = parser.parse(response.content)
 
+# LCEL chain:
+# Prompt → LLM → JSON Parser
 chain = template | model | parser
+
+
+# No input required because input_variables = []
 response = chain.invoke({})
 
+# response is now a Python dictionary
 print(response)
